777df4cf66481f4bdd29a7f1e49de352fd4fcd93
Yurii Filiushkin <yurii.filiushkin@gmail.com>
2024-04-17 21:17:53 +0300

    sonic improved

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,20 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  let partnerStatus = 'wife';
+  const getMarriageStatus = () => {
+    if (!isMarried) {
+      return 'I am not married';
+    }
 
-  if (sex === 'f') {
-    partnerStatus = 'husband';
-  }
+    const partner = sex === 'm' ? 'wife' : 'husband';
+
+    return `${partnerName} is my ${partner}`;
+  };
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
-      {!isMarried ? (
-        <p className="Person__partner">I am not married</p>
-      ) : (
-        <p className="Person__partner">{`${partnerName} is my ${partnerStatus}`}</p>
-      )}
+      <p className="Person__partner">{getMarriageStatus()}</p>
     </section>
   );
 };
